70ebd9f42bbb4cc3e4436df0392685e79d5d91e6
Soo Jung Kim <sokim@sokim-mn2.linkedin.biz>
2022-06-04 20:07:30 -0700

    wise

--- screen.py
+++ screen.py
@@ -3,6 +3,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 import time
 
 
@@ -13,6 +14,15 @@ search = browser.find_element(By.XPATH, '//*[@id="search-box-input"]')
 search.clear()
 search.send_keys("palo alto")
 search.send_keys(Keys.RETURN)
-time.sleep(4)
+time.sleep(5)
+select_house = Select(browser.find_element(By.XPATH, '//*[@id="sidepane-header"]/div/div[1]/form/div[3]/div/svg'))
+select.select_by_value("House")
+
+
+done_button = driver.find_element(By.XPATH,'//*[@id="sidepane-header"]/div/div[1]/form/div[3]/div[2]/div[1]/div/div[2]/div/button[2]/span')
+done_button.submit();
+time.sleep(7)
+
+
 
 browser.close()
