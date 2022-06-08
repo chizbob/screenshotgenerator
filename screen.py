bf9580183854c5bfaa61aa93e493e6dbed2bc978
Soo Jung Kim <sokim@sokim-mn2.linkedin.biz>
2022-06-07 23:08:34 -0700

    take screenshot and save

--- screen.py
+++ screen.py
@@ -2,27 +2,42 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 import time
+from PIL import Image
 
 
-browser= webdriver.Chrome()
-browser.get('http://www.redfin.com')
+#go to linkedin site
+driver= webdriver.Chrome()
+driver.get('http://www.linkedin.com')
 
-search = browser.find_element(By.XPATH, '//*[@id="search-box-input"]')
-search.clear()
-search.send_keys("palo alto")
-search.send_keys(Keys.RETURN)
-time.sleep(5)
-select_house = Select(browser.find_element(By.XPATH, '//*[@id="sidepane-header"]/div/div[1]/form/div[3]/div/svg'))
-select.select_by_value("House")
-
-
-done_button = driver.find_element(By.XPATH,'//*[@id="sidepane-header"]/div/div[1]/form/div[3]/div[2]/div[1]/div/div[2]/div/button[2]/span')
-done_button.submit();
+#login to the site
+driver.find_element(By.XPATH, '/html/body/nav/div/a[2]').click()
+driver.find_element(By.ID, 'username').send_keys('sokim@linkedin.com')
+driver.find_element(By.ID, 'password').send_keys('rlatnwjd12')
+driver.find_element(By.XPATH, '//*[@id="organic-div"]/form/div[3]/button').click()
 time.sleep(7)
 
+#take a screenshot
 
+driver.save_screenshot('home_us.png')
+screenshot = Image.open('home_us.png')
 
-browser.close()
+#change language setting
+# #go to the profile
+# search = browser.find_element(By.XPATH, '//*[@id="search-box-input"]')
+# search.clear()
+# search.send_keys("palo alto")
+# search.send_keys(Keys.RETURN)
+# time.sleep(5)
+#
+# #go to certifications, view all licenses and certifications
+# select_house = Select(browser.find_element(By.XPATH, '//*[@id="sidepane-header"]/div/div[1]/form/div[3]/div[1]/span'))
+# select.select_by_value("House")
+#
+# #screen capture and save
+
+#unit test
+#done_button.submit();
+time.sleep(2)
+driver.close()
